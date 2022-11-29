62d5733283d105aeaf1e68b028e06bdaec475391
Dinca2 <82816428+Dinca2@users.noreply.github.com>
2022-11-29 13:51:35 -0500

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 let User = require('../models/user');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 router.get('/', (req, res) => {
   User.find()
@@ -7,13 +9,74 @@ router.get('/', (req, res) => {
     .catch(err => res.status(400).json('Err: ' + err));
 });
 
-router.post('/signup', (req, res) => {
-  const username = req.body.username;
-  const newUser = new User({username});
+router.post("/signup", async (req, res) => {
+  const user = req.body;
   
-  newUser.save()
-  .then(() => res.json('User added!'))
-  .cath(err => res.status(400).json('Error: ' + err));
+  const takenEmail = await(User.findOne({email: user.email}));
+  const takenUsername = await(User.findOne({username: user.username}));
+  
+  if(takenEmail || takenUsername) {
+    res.json({message: "Username or Email has already been taken"});
+  } else {
+    user.password = await(bcrypt.hash(req.body.password,10));
+    
+    const dbUser =  new User({
+      username: user.username.toLowerCase(),
+      email:user.email.toLowerCase(),
+      password:user.password
+    });
+    
+    dbUser.save();
+    res.json({message: "Success"});
+  }
 });
 
+router.post("/login", async (req, res) => {
+  const userLoggingIn = req.body;
+  await User.findOne({username: userLoggingIn.username}).then(dbUser => {
+    if(!dbUser) {
+      return res.json({
+        message: "Invalid Username or Password"
+      });
+    }
+    bcrypt.compare(userLoggingIn.password, dbUser.password).then(isCorrect => {
+      if(isCorrect) {
+        const payload = {
+          email: dbUser.email,
+          username: dbUser.username
+        };
+        
+        jwt.sign(
+          payload,
+          process.env.JWT_SECRET,
+          {expiresIn: 86400},
+          (err,token) => {
+            if(err) return res.json({message:err});
+            return res.json({
+              message: "Success",
+              token: "Bearer " + token,
+            });
+          });
+        }
+      });
+    });
+});
+
+router.post("/getusername", async (req,res) => {
+  const token = req.headers["x-access-token"].split(' ')[1];
+  if(token) {
+     await jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+      if(err) return res.json({
+        isLoggedIn: false,
+        message: "Failed to Authenticate"
+      });
+      return res.json({username: decoded.username});
+    });
+  } else {
+    return res.json({
+      message: "Incorrect Token Given", isLoggedIn: false
+    });
+  }
+});
 module.exports = router;
+
