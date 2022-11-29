5a00470efa31d1ebdfd4f061b5e916517a76d40e
Dinca2 <82816428+Dinca2@users.noreply.github.com>
2022-11-29 13:46:30 -0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,20 @@
 import './Navbar.css';
-import AnimalReg from './AnimalReg';
 import {Link} from 'react-router-dom';
-import {useEffect, useState} from 'react';
-import {useNavigate} from 'react-router';
-
+import {useContext} from 'react';
+import {AuthContext} from '../auth/authContext.js'
 export default function Navbar () {
-
+  
+  const auth = useContext(AuthContext);
+  
+  function handleLogout() {
+    localStorage.removeItem("token");
+    auth.logout();
+  }
   return (
     <div className = "top-menu">
-        <nav migration_allowed="1" migrated="0" role="navigation">
-         <img src = "images/logo.png" className = "logo" alt = "logo" width="150"/>
+      <img src = "images/logo.jpg" alt = "logo" width="150"/>
+      <div className="widget-container">
+        <nav migration_allowed="1" migrated="0" role="navigation" className="nav-menu">
           <ul className="menu-ul">
             <li className = "menu-item">
               <Link to = "/about" className ="item_button">ABOUT </Link>
@@ -17,22 +22,36 @@ export default function Navbar () {
             <li className = "menu-item">
               <Link to = "/adopt" className ="item_button">ADOPT</Link>
             </li>
-            <li className = "menu-item">
-              <Link to ="/animalregister" id="animalRegBtn" >ANIMAL REGISTER</Link>
+            
+            {auth.isLoggedIn && (
+              <li className = "menu-item">
+                <Link to ="/animalregister" id="animalRegBtn" >Animal Register</Link>
+              </li>
+              )}
+              
+            {auth.isLoggedIn && (
+              <li className = "menu-item">
+                <Link to="/" onClick={() => handleLogout()} id="logoutBtn">Logout</Link>
+              </li>
+            )}
+            
+            {!auth.isLoggedIn && (
+              <li className = "menu-item-login">
+                <Link to = "/signup" id="signUpBtn">Sign Up</Link>
+              </li>
+            )}
+            {!auth.isLoggedIn && (
+            
+              <li className = "menu-item-login">
+                <Link to = "/login" id="loginBtn">Login</Link>
+              </li>
+            )}
+            <li className = "menu-contact">
+              <a href = "#contact" id ="item_contact">CONTACT</a>
             </li>
-            <li className = "menu-item-login">
-              <Link to = "/signup" id="signUpBtn">
-                <button class="signup-btn" role="button">Sign Up</button>
-              </Link>
-            </li>
-            <li className = "menu-item-login">
-              <Link to = "/login" id="loginBtn">
-                <button class="signin-btn" role="button">Sign In</button>
-              </Link>
-            </li>
-          
           </ul>
         </nav>
+      </div>
     </div>
   );
 }
