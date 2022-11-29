899d8c244f9b7f8b8be030bb18be0603ee3cc360
Dinca2 <82816428+Dinca2@users.noreply.github.com>
2022-11-29 13:45:01 -0500

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,24 +1,68 @@
 import './Login.css';
-import {useEffect} from 'react';
+import {useContext} from 'react';
 import {useNavigate} from 'react-router';
-
-export default function Login() {
-
+import {AuthContext} from '../auth/authContext.js'
+function Login() {
+  const navigate = useNavigate();
+  const auth = useContext(AuthContext);
+  console.log(auth);
+    async function handleLogin(e) {
+    e.preventDefault();
+    const form = e.target;
+    const user = {
+      username: form[0].value,
+      password: form[1].value,
+    }
+    
+    await fetch("/login", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify(user)
+    })
+    .then(res =>res.json())
+    .then(data => {localStorage.setItem("token", data.token)
+    });
+    
+    await fetch("/isUserAuth", {
+        method: "POST",
+        headers: {
+          "x-access-token": localStorage.getItem("token")
+        }
+      })
+      .then(res =>res.json())
+      .then(data => {
+        if(data.isLoggedIn) {
+          auth.login();
+          navigate("/about");
+        } else {
+          auth.logout();
+        }
+      });
+      
+  }
+    
+    
   return (
-      <div className="popup" id="login">
+      <div className="login-signup" id="login">
         <h1>Login</h1>
-        <form action="/login" method="POST">
+        <form onSubmit={event => handleLogin(event)}>
               
-          <label htmlFor="email"><b>Email</b></label>
-          <input type="text" placeholder="Enter Email" name="email" required/>
+          <label htmlFor="username"><b>Username</b></label>
+          <input type="text" placeholder="Enter username" name="username" required/>
           <br/>
           <label htmlFor="psw"><b>Password</b></label>
           <input type="password" placeholder="Enter Password" name="psw" required/>
           <br/>
-          <input type="submit" value="Submit" className="submit-btn"/>
+          <input type="submit" value="Submit"/>
              
         </form>
     </div>
   );
 }
+
+export default Login;
+  
+
   
